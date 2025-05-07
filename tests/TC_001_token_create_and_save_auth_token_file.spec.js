928bc54fab77465da11089004eded37054bd18e7
shihab0005 <shihab0000000000@gmail.com>
2025-05-08 00:31:03 +0600

    Update package.json file

--- tests/TC_001_token_create_and_save_auth_token_file.spec.js
+++ tests/TC_001_token_create_and_save_auth_token_file.spec.js
@@ -1,4 +1,4 @@
-import test, { expect } from "@playwright/test";
+import test from "@playwright/test";
 import { BASE_URL, USERNAME, PASSWORD } from "../Utils/config";
 import fs from "fs/promises";
 import { getToken } from "../Utils/tokenGenerate";
